bbb2fb85eab6d61c8c227a363054e5a6dd1c69d1
tracksuitdev <81776803+tracksuitdev@users.noreply.github.com>
2021-06-27 23:47:10 +0200

    Export multiple async combobox

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { useMultipleSelect } from "./use-multiple-select";
 export { useCombobox } from "./use-combobox";
 export { useMultipleCombobox } from "./use-multiple-combobox";
 export { useAsyncCombobox } from "./use-async-combobox";
+export { useMultipleAsyncCombobox } from "./use-multiple-async-combobox";
